9c06f7a05db3d1251533a2847c7111971428491a
fivepointbro <39099706+fivepointbro@users.noreply.github.com>
2021-02-04 15:28:24 -0400

    Add files via upload

--- _js/helpers.js
+++ _js/helpers.js
@@ -1,5 +1,45 @@
 export default class Helper {
 
+	file = './file.txt';
+	data = [];
+	
+	constructor() {
+		this.TEXT(this.file, null).then(text => this.data = text);
+	};
+	
+	textToObject(data) {
+	// this function is insane but it turns any text file with almost any formatting
+	// into a javascript object we can use to iterate and display it
+	// honestly, so dope
+	
+	let obj = data.split(/\n{2,}/).filter(el => el != null && el != '')
+	
+	obj = obj.map((person, ind) => {
+		return { 
+					id : ind, 
+					contents : person.split('\n').filter(el => el != null && el != '') 
+				}
+				}).map(person => { 
+				return { 
+							id : person.id, 
+							contents: person.contents.map(str => str.split(': ')) 
+						}
+				})
+				
+	obj = obj.forEach(person => person.contents.forEach(i => console.log(i)))
+	
+	return obj
+	};
+	
+	SPLIT(splitter, indx) { 
+	self = this;
+	
+	indx ? self.data = self.data[indx].split(splitter) : self.data = self.data.split(splitter);
+	self.data = self.data.filter(el => el != null && el != '');
+	
+	return self.data;
+	};
+
 	show(out, str) {
 		str ? str = str : str = 'Printed:';
 		console.log(str, out);
@@ -88,5 +128,18 @@ export default class Helper {
 				console.log('Failed to fetch HTML:', err)
 			});
 
+	};
+	
+	TEXT(file, pattern) {
+		return fetch(file)
+			.then(response => {
+				return response.text();
+			})
+			.then(text => {
+				return text;
+			})
+			.catch(err => {
+				console.log('Failed to fetch TEXT:', err);
+			});
 	};
 }	
