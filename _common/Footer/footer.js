c2360526a4754017854e3641fb80893b7e042c34
fivepointbro <39099706+fivepointbro@users.noreply.github.com>
2021-03-01 09:29:43 -0400

    Add files via upload
    Snazzed up the toggle button for dark mode

--- _common/Footer/footer.js
+++ _common/Footer/footer.js
@@ -2,9 +2,15 @@ export default class Footer extends HTMLElement {
 
     slug = 'footer';
     location = './_common/' + this.slug + '/'; // you shouldn't need to touch this unless you change folder structures
+	links = [];
+    loaded = false
 
     constructor() {
         super();
+        const self = this;
+        $.TEXT('./footer.txt').then(text => {
+            self.links = $.textToObject(text)
+        });
     };
 
     static get observedAttributes() {
@@ -12,12 +18,32 @@ export default class Footer extends HTMLElement {
     };
 
     connectedCallback() {
-        const menu = this;
-        this.getTemplate()
-            .then(function (response) {
-                const clone = response;
-                menu.appendChild(clone);
-            })
+        const self = this;
+        if (!self.loaded) {
+            // get the template, then the data, then display it all
+            $.HTML(this.location + this.slug + '.html')
+                .then(response => {
+                    const clone = response;
+                    self.appendChild(clone);
+                    self.loaded = true;
+                })
+                .then(r => {
+                    const Emerg = $.grab('#Em-links', self)
+                    const nonEm = $.grab('#non-Em-links', self)
+                    const other = $.grab('#other-links', self)
+                    self.links.forEach(link => {
+                        const type = link.type
+                        const li = $.make('li', 'nav-item')
+                        const a = $.make('a', 'nav-link', link.label.trim())
+                        a.setAttribute('href', `${link.url.trim()}`)
+                        li.appendChild(a)
+                        if (type == '12W-emergency') { Emerg.appendChild(li) }
+                        else if (type == 'non-emergency') { nonEm.appendChild(li) }
+                        else if (type == 'other') { other.appendChild(li) }
+                    })
+                    self.loaded = true
+                })
+        }
     };
 
     getTemplate() {
